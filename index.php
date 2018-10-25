4afc79089adfbb316b66402df02eeb5884269f77
Alan Rodrigues <alancrato@hotmail.com>
2018-10-25 09:54:07 -0300

    alteracoes em area de atuacao

--- index.php
+++ index.php
@@ -38,48 +38,48 @@
 <div class="pub">
     <div class="container">
         <div class="section">
-            <h1 class="header center darken-4-text">Área de Atuação</h1>
+            <h1 class="header center white-text">Áreas de Atuação</h1>
             <div class="row center">
-                <h5 class="header col s12 light">Escritório especializado em direito tributário e cível.</h5>
+                <h5 class="header col s12 light white-text">Escritório especializado em direito tributário e cível.</h5>
             </div>
             <!--   Icon Section   -->
             <div class="row">
                 <div class="col s12 m4">
                     <div class="icon-block">
-                        <h2 class="center light-blue-text">
+                        <!--<h2 class="center white-text">
                             <i class="material-icons">
                                 account_balance
                             </i>
-                        </h2>
-                        <h5 class="center">Civil</h5>
+                        </h2>-->
+                        <h5 class="center white-text">Civil</h5>
 
-                        <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                        <p class="light white-text">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
                     </div>
                 </div>
 
                 <div class="col s12 m4">
                     <div class="icon-block">
-                        <h2 class="center light-blue-text">
+                        <!--<h2 class="center white-text">
                             <i class="material-icons">
                                 how_to_reg
                             </i>
-                        </h2>
-                        <h5 class="center">Tributária</h5>
+                        </h2>-->
+                        <h5 class="center white-text">Tributária</h5>
 
-                        <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                        <p class="light white-text">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
                     </div>
                 </div>
 
                 <div class="col s12 m4">
                     <div class="icon-block">
-                        <h2 class="center light-blue-text">
+                        <!--<h2 class="center white-text">
                             <i class="material-icons">
                                 event
                             </i>
-                        </h2>
-                        <h5 class="center">Acessoria</h5>
+                        </h2>-->
+                        <h5 class="center white-text">Acessoria</h5>
 
-                        <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                        <p class="light white-text">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
                     </div>
                 </div>
             </div>
